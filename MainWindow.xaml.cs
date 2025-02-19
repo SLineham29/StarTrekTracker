7a35051ac7c650a54a5bad17fbf6e3ccf0104fd8
Slineham29 <samuel.lineham@hotmail.com>
2025-02-19 00:16:21 +0000

    Created the main menu for the program with a Windows 11-styled window using MicaWPF.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Net.Http;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,16 +10,48 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TMDbLib;
+using TMDbLib.Client;
+using TMDbLib.Objects.TvShows;
+using MicaWPF.Controls;
+using TMDbLib.Objects.Movies;
 
 namespace StarTrekTracker;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : MicaWindow
 {
+    private TMDbClient client;
+
+    private int[] showIDs = { 253, 655, 580, 1855, 314, 67198, 85949 };
+
+    private int filmID = 152;
+
     public MainWindow()
     {
         InitializeComponent();
+        string apiKey = File.ReadAllText("TMDB_API_Key.txt");
+        client = new TMDbClient(apiKey);
+
+        Image[] showImages = [imgTOS, imgTNG, imgDSN, imgV, imgE, imgD, imgP];
+
+        for (int i = 0; i < showIDs.Length; i++)
+        {
+            LoadShowImagesAsync(showIDs[i], showImages[i]);
+        }
+        LoadFilmImagesAsync(filmID, imgFilms);
+    }
+
+    private async Task LoadShowImagesAsync(int showID, Image showImage)
+    {
+        TvShow show = await client.GetTvShowAsync(showID);
+        string posterUrl = $"https://image.tmdb.org/t/p/w500{show.PosterPath}";
+        showImage.Source = new BitmapImage(new Uri(posterUrl));
+    }
+
+    private async Task LoadFilmImagesAsync(int filmID, Image filmImage)
+    {
+        Movie film = await client.GetMovieAsync(filmID);
+        string posterUrl = $"https://image.tmdb.org/t/p/w500{film.PosterPath}";
+        filmImage.Source = new BitmapImage(new Uri(posterUrl));
     }
 }
